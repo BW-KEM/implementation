b88d34a35b0844f1983427d0ed5a7016253e7742
Songlin Li <118235223+SonglinCrypto@users.noreply.github.com>
2026-08-16 22:21:23 +0800

    Add files via upload

---
+++ ref/params.h
@@ -0,0 +1,60 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#ifndef KYBER_K
+#define KYBER_K 3	/* Change this for different security strengths */
+#endif
+
+
+/* Don't change parameters below this line */
+#if   (KYBER_K == 2)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber512_ref_##s
+#elif (KYBER_K == 3)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber768_ref_##s
+#elif (KYBER_K == 4)
+#define KYBER_NAMESPACE(s) pqcrystals_kyber1024_ref_##s
+#else
+#error "KYBER_K must be in {2,3,4}"
+#endif
+
+#define KYBER_N 256
+#define KYBER_Q 3329
+
+#define KYBER_SYMBYTES 32   /* size in bytes of hashes, and seeds */
+#define KYBER_SSBYTES  32   /* size in bytes of shared key */
+
+#define KYBER_POLYBYTES		384
+#define KYBER_POLYVECBYTES	(KYBER_K * KYBER_POLYBYTES)
+
+#define KYBER_EQ 12
+#define KYBER_POLYVECCOMPRESSEDBYTES (KYBER_K * 288)    /* u k*256*ep/8 */
+
+#if KYBER_K == 2
+#define KYBER_ETA1 3
+#define KYBER_ETA2 3
+#define KYBER_POLYCOMPRESSEDBYTES    128
+#elif KYBER_K == 3
+#define KYBER_ETA1 2
+#define KYBER_ETA2 2
+#define KYBER_POLYCOMPRESSEDBYTES    128                /* v 256*eg/8 */
+#elif KYBER_K == 4
+#define KYBER_ETA1 2
+#define KYBER_ETA2 2
+#define KYBER_POLYCOMPRESSEDBYTES    160
+#endif
+
+
+#define KYBER_INDCPA_MSGBYTES       (KYBER_SYMBYTES)
+#define KYBER_INDCPA_PUBLICKEYBYTES (KYBER_POLYVECBYTES + KYBER_SYMBYTES)
+#define KYBER_INDCPA_SECRETKEYBYTES (KYBER_POLYVECBYTES)
+#define KYBER_INDCPA_BYTES          (KYBER_POLYVECCOMPRESSEDBYTES + KYBER_POLYCOMPRESSEDBYTES)
+
+#define KYBER_PUBLICKEYBYTES  (KYBER_INDCPA_PUBLICKEYBYTES)
+
+/* 32 bytes of additional space to save ID(pk) */
+#define PREFIXHASHBYTES 32
+
+#define KYBER_SECRETKEYBYTES  (KYBER_INDCPA_SECRETKEYBYTES + KYBER_INDCPA_PUBLICKEYBYTES + PREFIXHASHBYTES + KYBER_SYMBYTES)
+#define KYBER_CIPHERTEXTBYTES (KYBER_INDCPA_BYTES)
+
+#endif
